3065d38610b9c717e23d785cb76d4fb2476e836f
irladorhipov <irladorhipov@yandex.ru>
2023-07-27 22:00:24 +0400

    добавил сериалайзер, сделал тестовый запрос на получение книг

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    private BookRepository $bookRepository;
+
+    public function __construct(BookRepository $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
+    }
+
     #[Route('/')]
     public function root(): Response
     {
-        return $this->json(['test' => 'hello, world!']);
+        $books = $this->bookRepository->findAll();
+        return $this->json($books);
     }
 }
