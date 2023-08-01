3b58fbb040e6f3ad06260fb5e965a44ac0e211ed
irladorhipov <irladorhipov@yandex.ru>
2023-08-01 21:55:38 +0400

    Замокали репозиторий, написали свой первый тест

--- tests/BookCategoryServiceTest.php
+++ tests/BookCategoryServiceTest.php
@@ -2,13 +2,22 @@
 
 namespace App\Tests;
 
+use App\Entity\BookCategory;
+use App\Repository\BookCategoryRepository;
 use App\Service\BookCategoryService;
+use Doctrine\Common\Collections\Criteria;
 use PHPUnit\Framework\TestCase;
 
 class BookCategoryServiceTest extends TestCase
 {
     public function testSomething(): void
     {
+        $repository = $this->createMock(BookCategoryRepository::class);
+        $repository->expects($this->once())
+            ->method('findBy')
+            ->with([], ['title' => Criteria::ASC])
+            ->willReturn([(new BookCategory())->setTitle('Test')->setSlug('test')]);
+
         $this->assertTrue(true);
     }
 }
