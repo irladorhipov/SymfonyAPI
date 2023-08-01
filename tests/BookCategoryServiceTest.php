cd564a68936d2935a7d14bc046f9d74e2ba6d33e
irladorhipov <irladorhipov@yandex.ru>
2023-08-01 21:35:18 +0400

    Тест для категорий

---
+++ tests/BookCategoryServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use App\Service\BookCategoryService;
+use PHPUnit\Framework\TestCase;
+
+class BookCategoryServiceTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
