0c3b80e3b297c5a61276957a9a68d6989a49b13d
irladorhipov <irladorhipov@yandex.ru>
2023-08-02 23:19:58 +0400

    Добавили документацию и задукоментировали ENP Point

--- src/Controller/BookCategoryController.php
+++ src/Controller/BookCategoryController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Service\BookCategoryService;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\BookCategoryListResponse;
+use OpenApi\Annotations as OA;
 
 class BookCategoryController extends AbstractController
 {
@@ -16,7 +19,14 @@ class BookCategoryController extends AbstractController
         $this->bookCategoryService = $bookCategoryService;
     }
 
-    #[Route('api/v1/book/categories')]
+    /** 
+     * @OA\Response(
+     *      response=200, 
+     *      description="Return book categories",
+     *      @model(type=BookCategoryListResponse::class)
+     * )
+     */
+    #[Route(path: 'api/v1/book/categories', methods: ['GET'])]
     public function categories(): Response
     {
         return $this->json($this->bookCategoryService->getCategories());
