be1a4b0d29f37ca5f20373f71ab8bd9a801b457f
irladorhipov <irladorhipov@yandex.ru>
2023-08-03 21:43:09 +0400

    Доработка сущности

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -3,7 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use DateTimeInterface;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
@@ -15,6 +18,32 @@ class Book
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(type: 'simple_array')]
+    private array $authors;
+
+    #[ORM\Column(type: 'date')]
+    private DateTimeInterface $publicationDate;
+
+    #[ORM\Column(type: 'boolean', optins: ['default' => false])]
+    private bool $meap;
+
+    /**
+     * @var Collection<BookCategory>
+     */
+    #[ORM\ManyToMany(targetEntity: BookCategory::class)]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +58,72 @@ class Book
     public function setTitle(string $title): static
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getAuthors(): array
+    {
+        return $this->authors;
+    }
+
+    public function setAuthors(array $authors): self
+    {
+        $this->authors = $authors;
+
+        return $this; 
+    }
+
+    public function getMeap(bool $meap): bool
+    {
+        return $this->meap;
+    }
+
+    public function setMeap(bool $meap): self
+    {
+        $this->meap = $meap;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<BookCategory>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    /**
+     * @param Collection<BookCategory> $categories
+     * @return $this 
+     */
+    public function setCategories(Collection $categories): self
+    {
+        $this->categories = $categories;
 
         return $this;
     }
