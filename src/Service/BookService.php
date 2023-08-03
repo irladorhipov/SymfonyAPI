3d7a3d3739a60ce47c94a4be2be63bb507994ba6
irladorhipov <irladorhipov@yandex.ru>
2023-08-03 22:53:40 +0400

    Добавили метод для получения книги по категории

--- src/Service/BookService.php
+++ src/Service/BookService.php
@@ -2,7 +2,26 @@
 
 namespace App\Service;
 
+use App\Model\BookListResponse;
+use App\Repository\BookCategoryRepository;
+use App\Repository\BookRepository;
+
 class BookService
 {
-    
+    private $bookRepository;
+    private $bookCategoryRepository;
+
+    public function __construct(BookRepository $bookRepository, BookCategoryRepository $bookCategoryRepository)
+    {
+        $this->bookRepository = $bookRepository;
+        $this->bookCategoryRepository = $bookCategoryRepository;
+    }
+
+    public function getBookByCategory(int $categoryId): BookListResponse
+    {
+        $category = $this->bookCategoryRepository->find($category);
+        if (null === $category) {
+            
+        }
+    }
 }
