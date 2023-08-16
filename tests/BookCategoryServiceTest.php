50c4de58323f5ddfe3143d8a323d107a8c460078
irladorhipov <irladorhipov@yandex.ru>
2023-08-16 22:32:39 +0400

    Создали рефлексию для того чтобы избавится от сетера id

--- tests/BookCategoryServiceTest.php
+++ tests/BookCategoryServiceTest.php
@@ -8,17 +8,21 @@ use App\Model\BookCategoryListResponse;
 use App\Repository\BookCategoryRepository;
 use App\Service\BookCategoryService;
 use Doctrine\Common\Collections\Criteria;
-use PHPUnit\Framework\TestCase;
 
-class BookCategoryServiceTest extends TestCase
+class BookCategoryServiceTest extends AbstractTestCase
 {
+    const ENTITY_ID = 7;
+
     public function testSomething(): void
     {
+        $category = (new BookCategory())->setTitle('Test')->setSlug('test');
+        $this->setEntityId($category, self::ENTITY_ID);
+
         $repository = $this->createMock(BookCategoryRepository::class);
         $repository->expects($this->once())
             ->method('findBy')
             ->with([], ['title' => Criteria::ASC])
-            ->willReturn([(new BookCategory())->setId(8)->setTitle('Test')->setSlug('test')]);
+            ->willReturn([]);
 
         $service = new BookCategoryService($repository);
         $exptected = new BookCategoryListResponse([new BookCategoryListItem(8, 'Test', 'test')]);
