7af50e9c99a05a06a0e7000367d9df9a72f714ec
irladorhipov <irladorhipov@yandex.ru>
2023-08-16 22:37:25 +0400

    Правка рефлексии

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -12,7 +12,7 @@ abstract class AbstractTestCase extends TestCase
         $class = new ReflectionClass($entity);
         $property = $class->getProperty($idField);
         $property->setAccessible(true);
-        $property->setValue($value); 
+        $property->setValue($entity, $value); 
         $property->setAccessible(false);
     }
 }
